87950dd8018e1d41778ee12b08aec50fb152b88e
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-08-30 15:52:02 -0400

    Se exporto expo

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+
+import {ConverterContext} from './src/context/ConverterContext'
+import ConverterTab from './src/Navigation/Tab/ConverterTab'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ConverterContext>
+      <NavigationContainer>
+        <ConverterTab/>
+        <StatusBar style="light" hidden={true}/>
+      </NavigationContainer>
+    </ConverterContext>
+
   );
 }
 
