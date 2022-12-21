f6d9a448b325d04e8eef538a2fbcee9d17877d96
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-12-21 01:21:08 -0400

    feat: add api url and key to config.js

--- src/config/Key.js
+++
@@ -1,2 +0,0 @@
-const YoutubeKey = "AIzaSyDACAW9ZxNACK_qw5Sik-7I3KZlF2hPvgI"
-export default YoutubeKey;
---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+const youtubeApiUrl = 'https://www.googleapis.com/youtube/v3'
+
+const youtubeKey = "AIzaSyDACAW9ZxNACK_qw5Sik-7I3KZlF2hPvgI"
+
+export {youtubeApiUrl, youtubeKey}
