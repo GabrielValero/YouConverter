0edefc8886627b3b4473d9af002828e39d0000ba
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-12-27 03:00:37 -0400

    feat: change api (url) to get  video download url

--- src/utils/downloadSource.js
+++ src/utils/downloadSource.js
@@ -1,7 +1,29 @@
-export default async function downloadSource({id}){
-  return await fetch(`https://you-converter.herokuapp.com/downloadVideo?videoId=${id}`,{
+export async function downloadSource(id){
+	const link = `https://you-converter-backend.vercel.app/api/video?videoId=${id}`
+
+  return await fetch(link,{
     method: "GET",
     mode: 'cors',
   })
   .then(response=>response.json())
+
+}
+
+export default async function downloadSourceAlternative(id){
+	const link = `https://youtube-video-info.p.rapidapi.com/video_formats?video=${id}`
+
+	return await fetch(link,{
+    method: "GET",
+    headers: {
+	    'X-RapidAPI-Key': '97d003ab94msh8d3524b4213f269p14a2bejsn3ec75c0160be',
+	    'X-RapidAPI-Host': 'youtube-video-info.p.rapidapi.com'
+	  }
+  })
+  .then(response=>response.json())
+  .then(res=>{
+    console.log(res.AllFormats?.filter(format => format.Type === "mp3").sort((a,b) => a.Size < b.Size ? 1 : -1)[0].Link || res)
+  	return {
+      url: res.AllFormats.filter(format => format.Type === "mp3").sort((a,b) => a.Size < b.Size ? 1 : -1)[0].Link
+    }
+  })
 }
