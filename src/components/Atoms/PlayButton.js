8892f26d28e3fa6c96d3afa7d38d13388938ecc8
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-12-27 02:54:07 -0400

    feat: add card components for show results research

--- src/components/Atoms/PlayButton.js
+++ src/components/Atoms/PlayButton.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import {Image, View, StyleSheet} from 'react-native'
+import {Image, View, StyleSheet, Pressable} from 'react-native'
 
 import color from '../../config/colors.js'
+import useMusicPlayer from '../../hooks/useMusicPlayer'
+export default function PlayButton({song}){
 
-export default function PlayButton(){
+	const {setPlayList} = useMusicPlayer()
+
+	const onPress = ()=>{
+		setPlayList({song})
+	}
+
 	return(
-		<View style={styles.button}>
-			<Image source={require('../../../assets/PlayVector.png')} style={styles.image}/>
-		</View>	
+		<Pressable onPress={onPress}>
+			<View style={styles.button}>
+				<Image source={require('../../../assets/PlayVector.png')} style={styles.image}/>
+			</View>	
+		</Pressable>
 	)
 }
 
@@ -16,10 +25,11 @@ const styles = StyleSheet.create({
 		width: 44,
 		height: 44,
 		borderRadius: 44,
+		margin: 10,
 		backgroundColor: color.pastelRed,
 		position: 'absolute',
-		bottom: 10,
-		right: 10,
+		bottom: 0,
+		right: 0,
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center'
