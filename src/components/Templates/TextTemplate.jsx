6039d396f6a210259a5c2bc8e0cf21c649a633ad
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-12-27 03:02:21 -0400

    feat: add nowrap property to textTemplate component

--- src/components/Templates/TextTemplate.jsx
+++ src/components/Templates/TextTemplate.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import {Text, StyleSheet} from 'react-native'
 import colors from '../../config/colors'
 
-export default function TextTemplate({children, style, bold, title, small}){
+export default function TextTemplate({children, noWrap, style, bold, title, small}){
 
   const textStyles = [styles.text, title && styles.title,bold && styles.bold, small && styles.small, style]
 
-  return(
-    <Text style={textStyles}>{children}</Text>
-  )
+  return noWrap ? (
+    <Text numberOfLines={1} style={textStyles}>{children}</Text>
+  ) : <Text style={textStyles}>{children}</Text>
 }
 
 const styles = StyleSheet.create({
