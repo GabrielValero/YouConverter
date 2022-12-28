3c3d36ab3766b77a45cff75195c5db226647551e
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-12-28 04:15:07 -0400

    feat: add animation to add track button

--- src/components/Atoms/PlayButton.js
+++ src/components/Atoms/PlayButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Image, Pressable, View, StyleSheet} from 'react-native'
+import {Image, Pressable, View, StyleSheet, Animated} from 'react-native'
 
 import color from '../../config/colors.js'
 
@@ -7,17 +7,36 @@ import useMusicPlayer from '../../hooks/useMusicPlayer'
 
 export default function PlayButton({song}){
 
+
 	const {setPlayList} = useMusicPlayer()
 
+	
+	const rotateAnim = React.useRef(new Animated.Value(0)).current
+
+	const getRotationAnimation = ()=>{
+		const rotate = rotateAnim.interpolate({
+			inputRange: [0, 1],
+			outputRange: ["0deg", "360deg"]
+		});
+		return {rotate}
+	}
+	const timingAnimation = () => {
+        Animated.timing(rotateAnim, {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+        }).start()
+    }
 	const onPress = ()=>{
+		timingAnimation()
 		setPlayList({song})
 	}
 
 	return(
 		<Pressable onPress={onPress}>
-			<View style={styles.button}>
+			<Animated.View style={[styles.button, {transform: [getRotationAnimation()]}]}>
 				<Image source={require('../../../assets/PlayVector.png')} style={styles.image}/>
-			</View>	
+			</Animated.View>	
 		</Pressable>
 	)
 }
