88431436a27a54ceb8017f47afc3bed852db16d3
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2023-11-18 17:49:54 -0400

    Update: add splash screen and change useYoutube extension

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ActivityIndicator, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import TrackPlayer, { Capability } from 'react-native-track-player';
-
+import * as SplashScreen from 'expo-splash-screen';
 import {setupPlayer} from './src/services'
 
 import {ConverterProvider} from './src/context/ConverterContext'
 import {ReproductorProvider} from './src/context/ReproductorContext'
 import { SearchStack } from './src/Navigation/Stack/ConverterStack'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
@@ -20,7 +22,6 @@ export default function App() {
       setIsPlayerReady(isSetup);
       !isSetup && setup()
     }
-    console.log("Por aqui va")
     setup();
   }, []);
 
