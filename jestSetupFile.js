608506d2939ab2b6f5fd564a2a34c792b8e36f21
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-04 18:06:31 -0400

    Update jestSetupFile.js
    Se hizo el mock para 'react-native-track-player'

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,3 +1,39 @@
 jest.mock("@react-native-async-storage/async-storage", () =>
 	require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
 );
+
+jest.mock('react-native-track-player', () => {
+	return {
+	  addEventListener: jest.fn(),
+	  registerEventHandler: jest.fn(),
+	  registerPlaybackService: jest.fn(),
+	  setupPlayer: jest.fn(),
+	  destroy: jest.fn(),
+	  updateOptions: jest.fn(),
+	  add: jest.fn(),
+	  remove: jest.fn(),
+	  skip: jest.fn(),
+	  skipToNext: jest.fn(),
+	  skipToPrevious: jest.fn(),
+	  removeUpcomingTracks: jest.fn(),
+	  // playback commands
+	  reset: jest.fn(),
+	  play: jest.fn(),
+	  pause: jest.fn(),
+	  stop: jest.fn(),
+	  seekTo: jest.fn(),
+	  setVolume: jest.fn(),
+	  setRate: jest.fn(),
+	  // player getters
+	  getQueue: jest.fn(),
+	  getTrack: jest.fn(),
+	  getCurrentTrack: jest.fn(),
+	  getVolume: jest.fn(),
+	  getDuration: jest.fn(),
+	  getPosition: jest.fn(),
+	  getBufferedPosition: jest.fn(),
+	  getState: jest.fn(),
+	  getRate: jest.fn(),
+	  ProgressComponent: jest.fn()
+	};
+  });
