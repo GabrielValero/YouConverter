2c02397724572a2a636b923fa34f8cf4d3a472f1
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-09-21 14:37:02 -0400

    Se cambio el nlmbre de item a track y se aplico formato

--- src/components/Molecules/ListCard.tsx
+++ src/components/Molecules/ListCard.tsx
@@ -1,57 +1,76 @@
 import React from 'react'
-import {View, Pressable,  StyleSheet} from 'react-native'
+import { View, Pressable, StyleSheet } from 'react-native'
 import ImageTemplate from '../Templates/ImageTemplate'
 import TextTemplate from '../Templates/TextTemplate'
 
-import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
 
-import PlayButton from '../Atoms/PlayButton';
+import PlayButton from '../Atoms/PlayButton'
 
-import DownloadButton from '../Atoms/DownloadButton';
-import { TrackData } from '../../types';
-import formatTime from '../../utils/formatTime';
+import DownloadButton from '../Atoms/DownloadButton'
+import { TrackData } from '../../types'
+import formatTime from '../../utils/formatTime'
 
 type Prop = {
-    item: TrackData
+    track: TrackData
 }
-export default function ListCard({item}: Prop){
-    
-    const title = item.title
-    const artist = item.artist
-    const duration = item.duration
-    return item && (
-        <View style={[styles.container, {maxWidth: "100%", marginVertical:15}]}>
-            <View style={styles.containerImage}>
-                <ImageTemplate url={item.artwork} style={styles.image} cover coverWidth coverHeight/>
+export default function ListCard({ track }: Prop) {
+    const title = track.title
+    const artist = track.artist
+    const duration = track.duration
+    return (
+        track && (
+            <View
+                style={[
+                    styles.container,
+                    { maxWidth: '100%', marginVertical: 15 },
+                ]}
+            >
+                <View style={styles.containerImage}>
+                    <ImageTemplate
+                        url={track.artwork}
+                        style={styles.image}
+                        cover
+                        coverWidth
+                        coverHeight
+                    />
+                </View>
+                <View style={styles.containerText}>
+                    <TextTemplate textMedium noWrap numberOfLines={2}>
+                        {title}
+                    </TextTemplate>
+                    <TextTemplate
+                        textSmall
+                        noWrap
+                    >{`Duración: ${formatTime(duration)}`}</TextTemplate>
+                    <TextTemplate textSmall noWrap>
+                        {artist}
+                    </TextTemplate>
+                </View>
+                <View style={[styles.container]}>
+                    <DownloadButton track={track} />
+                    <PlayButton track={track} />
+                </View>
             </View>
-            <View style={styles.containerText}>
-                <TextTemplate textMedium noWrap numberOfLines={2}>{title}</TextTemplate>
-                <TextTemplate textSmall noWrap>{`Duración: ${formatTime(duration)}`}</TextTemplate>
-                <TextTemplate textSmall noWrap>{artist}</TextTemplate>
-            </View>
-            <View style={[styles.container]}>
-                <DownloadButton item={item}/>
-                <PlayButton track={item}/>
-            </View>
-        </View>
+        )
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
     },
-    containerImage:{
+    containerImage: {
         width: 50,
         height: 50,
-        marginRight: 14
+        marginRight: 14,
     },
-    containerText:{
-        width: "50%"
+    containerText: {
+        width: '50%',
     },
-    image:{
-        borderRadius: 7
-    }
+    image: {
+        borderRadius: 7,
+    },
 })
